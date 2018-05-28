d872ebdb5f5e8df13e2a35c1584dbd13eb78ecb7
山野 真善志 <yamano@psid.co.jp>
2018-05-28 16:21:40 +0900

    マックエージェンシーwebアップデータ初回

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+ var elem = document.querySelector('.carousel');
+  var instance = M.Carousel.init(elem, options);
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+      
